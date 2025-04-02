7c1b4367951dea6fe6934354abddd4e73b64b80a
amitdubey88 <amitdubey19990808@gmail.com>
2025-04-02 16:15:21 +0530

    resume wxtra animation delete

--- script.js
+++ script.js
@@ -329,26 +329,7 @@ document.querySelectorAll('.contact-info, .contact-form').forEach((element, inde
     contactObserver.observe(element);
 });
 
-// 3D Card Effect
-document.querySelectorAll('.card-3d').forEach(card => {
-    card.addEventListener('mousemove', function (e) {
-        const rect = this.getBoundingClientRect();
-        const x = e.clientX - rect.left;
-        const y = e.clientY - rect.top;
 
-        const centerX = rect.width / 2;
-        const centerY = rect.height / 2;
-
-        const rotateX = (y - centerY) / 20;
-        const rotateY = (centerX - x) / 20;
-
-        this.style.transform = `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale3d(1.02, 1.02, 1.02)`;
-    });
-
-    card.addEventListener('mouseleave', function () {
-        this.style.transform = 'perspective(1000px) rotateX(0) rotateY(0) scale3d(1, 1, 1)';
-    });
-});
 
 // Particle Effect on Button Click
 function createParticles(e, button) {
